84b3b1bd8f74f4ede731549849ed73432e3c1259
tarun <tonystark142333@gmail.com>
2026-01-12 14:58:39 +0530

    adding permanent memory

---
+++ terraform/iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name = "jenkins-profile-automated"
+  role = aws_iam_role.jenkins_role.name
+}
